4a3843da60299233d9a6d843d2b681918cddda00
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-05-13 11:58:37 -0400

    removed footer from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
